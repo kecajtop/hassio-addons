4a72f801255f85c393e6f79214822bbe42166acb
JKBI <kecajtop@wp.pl>
2025-02-26 08:57:41 +0100

    update

--- apache2/run.sh
+++ apache2/run.sh
@@ -174,8 +174,8 @@ mkdir /usr/lib/php84/modules/opcache
 
 #git clone https://github.com/kecajtop/mqtt_php.git  $webrootdocker
 
+composer install --working-dir=$webrootdocker
 composer require php-mqtt/client -vvv
-
 echo "Here is your web file architecture."
 ls -l $webrootdocker
 ls /var/www/
